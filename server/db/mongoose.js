442c2416d8d3819f1c424f730f3ccdfdbef4b1e6
Lihat Ervin <erwyn86@yahoo.com>
2018-01-22 12:23:11 +0200

    Added database with heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://dbuser:M2305lab##@ds111608.mlab.com:11608/todoapp');
 
 module.exports = {
   mongoose,
-  
+
 }
